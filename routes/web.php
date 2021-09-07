2bddd0570bf718a0bac17fe75beaae80fb157dac
Ikimune <s-kaho.ikimune@rakuten.com>
2021-09-07 17:01:14 +0900

    第三章第八部

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,21 @@ Route::get('/', function () {
 });
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-
+    Route::post('news/create', 'Admin\NewsController@create'); 
     Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/edit', 'Admin\NewsController@add')->middleware('auth');
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::get('/home', 'HomeController@index')->name('home');});
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+    Route::get('/home', 'HomeController@index')->name('home');
+});
 // Route::get('xxx', 'AAAController@bbb');
-//Auth::routes();
+Auth::routes();
 ?>
 
 
